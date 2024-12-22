b89bfe358a6fb07156ee53e29cd5caf0eb1f067a
poojitha0930 <148348934+poojitha0930@users.noreply.github.com>
2024-12-22 12:27:12 +0530

    Time: 0 ms (100%), Space: 41.6 MB (44.12%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        while (root != null) {
+            if (root.left == null) {
+                ans.add(root.val);
+                root = root.right;
+            } else {
+                TreeNode prev = root.left;
+                while (prev.right != null && prev.right != root) {
+                    prev = prev.right;
+                }
+                if (prev.right == null) {
+                    ans.add(root.val);
+                    prev.right = root;
+                    root = root.left;
+                } else {
+                    prev.right = null;
+                    root = root.right;
+                }
+            }
+        }
+        return ans;
+    }
+}
